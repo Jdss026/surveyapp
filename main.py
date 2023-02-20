0496c91fa4b4d7a7072f03780e0f46c1ada43493
Jdss026 <jdss026@gmail.com>
2023-02-20 10:24:41 -0300

    translated to ptbr & fix 'unique_id' dual value bug

--- main.py
+++ main.py
@@ -23,24 +23,25 @@ bucket_name = 'surveyappproject-bucket'
 # Define a function to create the CSV filename based on the survey name and current date/time
 def create_csv_filename(survey_name):
     timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
+    global unique_id
     unique_id = str(uuid.uuid4())[:8]
     return f'{survey_name}-{timestamp}-{unique_id}.csv'
 
 # Define the survey questions and input fields
-survey_name = 'My Survey'
-q1 = st.text_input('What is your name?')
-q2 = st.selectbox('What is your favorite color?', ['Red', 'Green', 'Blue'])
-q3 = st.slider('How many hours do you work in a day?', min_value=0, max_value=24)
+survey_name = 'Questionário'
+q1 = st.text_input('Qual seu nome?')
+q2 = st.selectbox('Qual sua cor favorita?', ['Vermelho', 'Verde', 'Azul'])
+q3 = st.slider('Quantas horas você trabalha por dia?', min_value=0, max_value=24)
 
 # Add a section for additional comments
-st.subheader('Additional Comments')
-num_comments = st.number_input('How many additional comments do you have?', min_value=0, max_value=10, step=1)
+st.subheader('Comentários Adicionais')
+num_comments = st.number_input('Quantos comentários adicionais você têm?', min_value=0, max_value=10, step=1)
 comments = []
 for i in range(num_comments):
-    comments.append(st.text_input(f'Comment {i+1}'))
+    comments.append(st.text_input(f'Comentário {i+1}'))
 
 # Add a submit button to submit the survey
-submit_button = st.button('Submit')
+submit_button = st.button('Enviar')
 
 # Process the survey when the submit button is clicked
 if submit_button:
@@ -63,7 +64,7 @@ if submit_button:
     # Append the survey response to the CSV file
     with open('temp.csv', 'w', newline='') as f:
         writer = csv.writer(f)
-        unique_id = str(uuid.uuid4())[:8]
+        # unique_id = str(uuid.uuid4())[:8]
         response.insert(0, unique_id)
         writer.writerow(response)
     s3.upload_file('temp.csv', bucket_name, csv_file_name)
